6ef3707209c29411fe004716d91c4efbea548e27
ArchiouBlueCo <achiroublueco@gmail.com>
2024-12-13 13:43:58 +0000

    Update apiService.js

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const apiUrl = "https://intrepidscouting.onrender.com";
-const apiUrl = "http://localhost:3000";
+const apiUrl = "https://archioubluecoscouting.onrender.com";
 
 
 // Base URL for your backend API 
